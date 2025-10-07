5ebd7427de5c4c2a25a5fb2568812859d9b5bafb
Nambu Kamali <nambukamali@gmail.com>
2025-10-07 23:20:23 +0530

    Add files via upload

---
+++ LoginData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace MVCcrudoperation.Models
+{
+    public class LoginData
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-EHPCDIL\SQLEXPRESS02;Initial Catalog=nambu;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        public int id { get; set; }
+        public string username { get; set; }
+
+        public string save(LoginData d)
+        {
+            if(string.IsNullOrEmpty(d.username))
+            {
+                return "Username Required";
+            }
+            try
+            {
+                string safeUser = d.username.Replace("'", "''");
+                con.Open();
+                cmd = new SqlCommand("insert into login values(" + d.id + ",'" + safeUser + "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                return "Login success";
+            }
+            catch(Exception ex)
+            {
+                con.Close();
+                return "Error:" + ex.Message;
+            }
+        }
+    }
+}
