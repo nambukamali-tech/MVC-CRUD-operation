9f12617bf92e68e228427da4f192c76d003a3d2f
Nambu Kamali <nambukamali@gmail.com>
2025-10-07 23:18:43 +0530

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,29 @@
+﻿using MVCcrudoperation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCcrudoperation.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(LoginData d)//Passing parameter for function call
+        {
+            LoginData d1 = new LoginData();//Creating object for model class
+            ViewBag.d2 = d1.save(d);
+            return View();
+
+        }
+
+    }
+}
