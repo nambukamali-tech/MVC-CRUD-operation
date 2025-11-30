3457f1a9d472c0ff8e0ee79a0040e7b750357c52
Nambu Kamali <nambukamali@gmail.com>
2025-11-30 21:24:04 +0530

    Add files via upload

---
+++ DataStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice10.Models
+{
+    public class DataStorage
+    {
+        public string name { get; set; }
+       
+        public string area { get; set; }
+        public int salary { get; set; }
+    }
+}
